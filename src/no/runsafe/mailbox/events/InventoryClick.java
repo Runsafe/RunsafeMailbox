e32491fea18270d9853574aa109afe85652a82a7
Kruithne <kruithne@gmail.com>
2013-05-06 19:41:35 +0100

    Check the correct inventory

--- src/no/runsafe/mailbox/events/InventoryClick.java
+++ src/no/runsafe/mailbox/events/InventoryClick.java
@@ -2,6 +2,7 @@ package no.runsafe.mailbox.events;
 
 import no.runsafe.framework.event.inventory.IInventoryClick;
 import no.runsafe.framework.server.event.inventory.RunsafeInventoryClickEvent;
+import no.runsafe.framework.server.inventory.RunsafeAnvilInventory;
 import no.runsafe.framework.server.item.RunsafeItemStack;
 import no.runsafe.framework.server.player.RunsafePlayer;
 
@@ -18,7 +19,7 @@ public class InventoryClick implements IInventoryClick
 			if (displayName != null)
 			{
 				RunsafePlayer player = event.getWhoClicked();
-				if (player.isViewingAnvil())
+				if (event.getInventory() instanceof RunsafeAnvilInventory)
 				{
 					if (displayName.startsWith("Mail Package #"))
 					{
