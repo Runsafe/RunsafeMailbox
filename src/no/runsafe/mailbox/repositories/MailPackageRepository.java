8ae540a599dff08c9e8df3622ffddbc387f603bd
Kruithne <kruithne@gmail.com>
2013-05-21 04:33:46 +0100

    Changed to return an integer.

--- src/no/runsafe/mailbox/repositories/MailPackageRepository.java
+++ src/no/runsafe/mailbox/repositories/MailPackageRepository.java
@@ -41,16 +41,7 @@ public class MailPackageRepository extends Repository
 		// Science?
 		Map<String, Object> data = this.database.QueryRow("SELECT LAST_INSERT_ID() AS ID FROM mail_packages");
 		if (data != null)
-		{
-			if (data.get("ID") instanceof Integer)
 				return (Integer) data.get("ID");
-
-			if (data.get("ID") instanceof Long)
-			{
-				long theLong = (Long) data.get("ID");
-				return (int) theLong;
-			}
-		}
 
 		return 0;
 	}
