bffdfcdf362e415d58908953f8d30bdc818ad7da
Kruithne <kruithne@gmail.com>
2013-06-10 22:40:26 +0100

    Updated to reflect recent item API changes

--- src/no/runsafe/mailbox/events/InventoryClick.java
+++ src/no/runsafe/mailbox/events/InventoryClick.java
@@ -3,7 +3,6 @@ package no.runsafe.mailbox.events;
 import no.runsafe.framework.event.inventory.IInventoryClick;
 import no.runsafe.framework.server.event.inventory.RunsafeInventoryClickEvent;
 import no.runsafe.framework.server.inventory.RunsafeAnvilInventory;
-import no.runsafe.framework.server.item.RunsafeItemStack;
 import no.runsafe.framework.server.item.meta.RunsafeMeta;
 import no.runsafe.framework.server.player.RunsafePlayer;
 
@@ -12,11 +11,12 @@ public class InventoryClick implements IInventoryClick
 	@Override
 	public void OnInventoryClickEvent(RunsafeInventoryClickEvent event)
 	{
-		RunsafeItemStack item = event.getCurrentItem();
+		RunsafeMeta item = event.getCurrentItem();
+
 		if (item == null || !item.hasItemMeta() || !(event.getInventory() instanceof RunsafeAnvilInventory))
 			return;
 
-		if (((RunsafeMeta)item).getDisplayName().startsWith("Mail Package #"))
+		if (item.getDisplayName().startsWith("Mail Package #"))
 		{
 			RunsafePlayer player = event.getWhoClicked();
 			player.sendColouredMessage("&cYou cannot do that.");
