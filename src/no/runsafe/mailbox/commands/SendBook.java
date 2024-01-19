c4e0d6c6ed292c10bceb0046dbcc76d5c964af7e
Centralan <Jessegauthier070296@gmail.com>
2024-01-19 15:28:22 -0500

    Update SendBook.java

--- src/no/runsafe/mailbox/commands/SendBook.java
+++ src/no/runsafe/mailbox/commands/SendBook.java
@@ -27,7 +27,7 @@ public class SendBook extends PlayerCommand
 			return null;
 
 		if (player.getName().equalsIgnoreCase(executor.getName()))
-			return "&cYou cannot mail things to yourself.";
+			return "&cYou cannot send parcels to yourself.";
 
 		if (!this.mailHandler.hasMailBookCost(executor))
 			return "&cYou do not have enough money to send a book. Sending books costs " + this.mailHandler.getMailBookCostText() + ".";
